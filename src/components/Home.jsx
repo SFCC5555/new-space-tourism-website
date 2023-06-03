d1fe308012ced1b46fc949c82ef70418eca69b9a
SFCC5555 <sfcc.5555@gmail.com>
2023-06-03 18:37:06 -0500

    styles to button explore

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,9 +8,12 @@ const Home = () => {
             <h1>SPACE</h1>
             <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
         </section>
-        <div>
-            <Link to={'/DESTINATION'} >EXPLORE</Link>
+        <div className='flex items-center justify-center w-80 h-80 rounded-full bg-white/0 hover:w-80 hover:h-80 hover:bg-white/50 transition duration-1000'>
+          <div className='flex items-center justify-center w-60 h-60 bg-white text-black rounded-full cursor-pointer'>
+              <Link to={'/DESTINATION'} >EXPLORE</Link>
+          </div>
         </div>
+
     </section>
   )
 }
