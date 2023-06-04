ac83fd4c2002f8eaf6085d78758236cd3a9b3e2b
sfcc5555 <sfcc.5555@gmail.com>
2023-06-04 15:42:48 -0500

    crew section update

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const Crew = () => {
   return (
-    <section className='crew w-full absolute top-0 sm:h-screen px-5 pt-28 pb-16 sm:pb-0 flex flex-col items-center text-center gap-12 sm:px-14 md:px-36'>
+    <section className='crew w-full absolute top-0 sm:h-screen px-5 pt-28 pb-16 sm:pb-0 flex flex-col items-center text-center gap-12 sm:px-14 lg:px-36'>
       <Outlet />
     </section>
   )
--- src/components/Technology .jsx
+++ src/components/Technology .jsx
@@ -2,7 +2,7 @@ import '../styles/Technology.css';
 
 const Technology = () => {
   return (
-    <section className='technology w-full absolute top-0 h-screen px-5 pt-28 pb-16 flex flex-col items-center text-center gap-12 sm:flex-row sm:justify-between sm:gap-2 sm:px-14 md:px-36'>
+    <section className='technology w-full absolute top-0 h-screen px-5 pt-28 pb-16 flex flex-col items-center text-center gap-12 sm:flex-row sm:justify-between sm:gap-2 sm:px-14 lg:px-36'>
 
 
     </section>
