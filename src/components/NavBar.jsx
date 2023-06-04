84bdc53104b1f65d2f6d8dc313cfafaabba1320c
MrAckerman5555 <sfcc.93@hotmail.com>
2023-06-04 01:19:29 -0500

    navbar update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,10 +4,10 @@ import burgerIcon from '../assets/shared/icon-hamburger.svg';
 
 const NavBar = () => {
   return (
-    <nav className ='fixed w-full flex items-center justify-between py-3 px-5 z-10'>
-        <span className='w-12 h-12' style={{backgroundImage:`url(${logo})`}} />
+    <nav className ='fixed w-full flex items-center justify-between p-7 sm:p-10 z-10'>
+        <span className='w-10 h-10 sm:w-12 sm:h-12' style={{backgroundImage:`url(${logo})`}} />
         <Menu />
-        <span className='w-8 h-8' style={{backgroundImage:`url(${burgerIcon})`}} />
+        <span className='w-7 h-7 cursor-pointer sm:hidden' style={{backgroundImage:`url(${burgerIcon})`}} />
     </nav>
 )}
 
