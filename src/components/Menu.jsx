aa0188666af5b48a7940f95034f6910beb5176e9
sfcc5555 <sfcc.5555@gmail.com>
2023-06-04 15:57:28 -0500

    crew section update

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,7 +5,7 @@ import '../styles/Menu.css'
 
 const Menu = ({ menuStatus,setMenuStatus }) => {
   return (
-    <section className={`menu ${!menuStatus&&'hidden'} sm:flex absolute sm:relative h-screen p-7 pr-0 sm:py-0 sm:px-5 gap-5 font2 text-sm w-1/2 sm:w-3/4 md:w-96 right-0 top-0 sm:h-auto z-20 flex flex-col sm:flex-row sm:justify-center bg-white/5 backdrop-blur-3xl sm:backdrop-blur-0`} >
+    <section className={`menu ${!menuStatus&&'hidden'} sm:flex absolute sm:relative min-h-screen sm:min-h-0 h-screen p-7 pr-0 sm:py-0 sm:px-5 gap-5 font2 text-sm w-1/2 sm:w-3/4 md:w-96 right-0 top-0 sm:h-auto z-20 flex flex-col sm:flex-row sm:justify-center bg-white/5 backdrop-blur-3xl sm:backdrop-blur-0`} >
         <span onClick={()=>setMenuStatus(false)} className='w-5 h-5 mr-7 self-end mb-12 cursor-pointer sm:hidden' style={{backgroundImage:`url(${closeIcon})`}} />
         <NavLink onClick={()=>setMenuStatus(false)} className='navLink' to={'/'} ><span>00</span> HOME</NavLink>
         <NavLink onClick={()=>setMenuStatus(false)} className='navLink' to={'/DESTINATION'} ><span>01</span> DESTINATION</NavLink>
