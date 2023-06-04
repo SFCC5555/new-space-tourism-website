f1a610f90aed6ad45c92c22b2bbd4aae6d8769b9
sfcc5555 <sfcc.5555@gmail.com>
2023-06-04 02:09:01 -0500

    a

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,10 +9,10 @@ const Home = () => {
             <h1 className='text-8xl font1 md:text-9xl' >SPACE</h1>
             <p className='font2 colorLight sm:text-xl' >Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
         </section>
-        <div className='flex items-center justify-center w-60 h-60 md:w-72 md:h-72' >
-          <div className='exploreButtonContainer flex items-center justify-center w-40 h-40 md:w-52 md:h-52 rounded-full' >
+        <div className='flex items-center justify-center w-52 h-52 md:w-64 md:h-64' >
+          <div className='exploreButtonContainer flex items-center justify-center w-32 h-32 md:w-48 md:h-48 rounded-full' >
           <Link to={'/DESTINATION'} >
-            <div className='flex items-center justify-center text-2xl md:text-3xl font1 w-40 h-40 md:w-52 md:h-52 bg-white colorDark rounded-full cursor-pointer'>
+            <div className='flex items-center justify-center text-2xl md:text-3xl font1 w-32 h-32 md:w-48 md:h-48 bg-white colorDark rounded-full cursor-pointer'>
               EXPLORE
             </div>
           </Link>
